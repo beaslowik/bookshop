fd3a64507f96331e527140943180c7de465bd6cd
Beata <bebem@poczta.onet.pl>
2022-05-21 22:01:00 +0200

    feat: book list v. 2

--- script/script.js
+++ script/script.js
@@ -5,36 +5,46 @@ fetch('books.json')
     .then(data => {
         let myData = data;
 
+        const booksList = document.createElement('section');
+        booksList.setAttribute("id", "books-list");
+        document.querySelector('main').appendChild(booksList);
+
         for (let index = 0; index < myData.length; index++) {
 
-            const node = document.createElement("article");
+            const article = document.createElement("article");
             const wrapper = document.createElement("div");
+
+            const image = document.createElement('img');
             const author = document.createElement("h2");
             const title = document.createElement("h3");
-            const description = document.createElement("p");
+            const price = document.createElement('span');
+            const more = document.createElement('button');
+            const addToBag = document.createElement('button');
 
 
-            const nodeAuthor = document.createTextNode(myData[index].author + ' ');
-            const nodeTitle = document.createTextNode(myData[index].title);
-            const nodeDescription = document.createTextNode(myData[index].description);
+            image.src = myData[index].imageLink;
+            author.textContent = myData[index].author + ' ';
+            title.textContent = myData[index].title;
+            price.textContent = myData[index].price + ' $';
+            more.textContent = 'Show more';
+            more.classList.add('show-more');
+            addToBag.textContent = 'Add to bag'
 
-
-            // + ' ' + '\"' + myData[index].title + '\" ' + myData[index].price + ' $ ' + myData[index].description);
-            const imgNode = document.createElement('img');
-
-
-            imgNode.src = myData[index].imageLink;
-
-            // node.appendChild(textnode);
-            document.getElementById("myList").appendChild(node).appendChild(imgNode);
-            document.getElementById("myList").appendChild(node).appendChild(wrapper).appendChild(author).appendChild(nodeAuthor);
-            document.getElementById("myList").appendChild(node).appendChild(wrapper).appendChild(title).appendChild(nodeTitle);
-            document.getElementById("myList").appendChild(node).appendChild(wrapper).appendChild(description).appendChild(nodeDescription);
-            // document.getElementById("myList").appendChild(node).appendChild(giveSpace);
-            // document.getElementById("myList").appendChild(imgNode);
-            // document.getElementById("myList").appendChild(textnode);
-
-
+            document.getElementById("books-list").appendChild(article).appendChild(image);
+            document.getElementById('books-list').appendChild(article).appendChild(wrapper).appendChild(author);
+            document.getElementById("books-list").appendChild(article).appendChild(wrapper).appendChild(title);
+            document.getElementById("books-list").appendChild(article).appendChild(wrapper).appendChild(price);
+            document.getElementById("books-list").appendChild(article).appendChild(wrapper).appendChild(more);
+            document.getElementById("books-list").appendChild(article).appendChild(wrapper).appendChild(addToBag);
         }
 
+        function showMore() {
+            for (let index = 0; index < myData.length; index++) {
+                alert(myData[index].description);
+
+            }
+        }
+        const buttonShowMore = document.getElementsByClassName('show-more');
+        buttonShowMore[0].addEventListener('click', showMore);
+        console.log(buttonShowMore);
     });
