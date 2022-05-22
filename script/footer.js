53748215ac0d165ce5ee61c1c3d09a65552eb019
Beata <bebem@poczta.onet.pl>
2022-05-23 00:03:01 +0200

    feat: use DocumentFragment in footer

--- script/footer.js
+++ script/footer.js
@@ -1,7 +1,10 @@
+const footerFragment = new DocumentFragment();
+
 const footer = document.createElement('footer');
 footer.classList.add('page-footer')
-document.querySelector('body').appendChild(footer);
 
 const copyRights = document.createElement('span');
 copyRights.innerText = 'Copyrights: Your Bookshop 2022'
-document.querySelector('footer').appendChild(copyRights);
+
+footerFragment.appendChild(footer).appendChild(copyRights);
+document.querySelector('body').appendChild(footerFragment);
